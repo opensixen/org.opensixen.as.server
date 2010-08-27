0c5574144b322dfdf71d913550468940a6b970f5
Eloy Gomez <eloy@indeos.es>
2010-08-27 17:42:22 +0000

    Añadimos IApplication al servidor de aplicaciones para que se arranque al iniciar.

---
+++ src/org/opensixen/as/server/ApplicationServer.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.opensixen.as.server;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * @author harlock
+ *
+ */
+public class ApplicationServer implements IApplication {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
